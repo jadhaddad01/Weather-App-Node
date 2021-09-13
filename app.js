e3ee448313e0dea7120338dcd9de78b7399d27c5
awdad <adwadawd>
2021-09-13 11:19:15 -0400

    request lattitude and longitude

--- app.js
+++ app.js
@@ -7,3 +7,10 @@ request({ url: url, json: true }, (error, response) => {
     console.log(data.weather_descriptions[0]);
     console.log('It is currently ' + data.temperature + ' degrees out. It feels like ' + data.feelslike + ' degrees out');
 });
+
+const url1 = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZ29tb2dpczgzOCIsImEiOiJja3Rpcnc0dWkxNTJ0Mm5tcHdrN2x3eGJjIn0.9S7zbuSe1LyE0Z_WMBcEpw&limit=1';
+
+request({ url: url1, json: true }, (error, response) => {
+    const data = response.body.features[0];
+    console.log(data.text + "'s lattitude is " + data.center[0] + ' and its longitude is ' + data.center[1]);
+});
